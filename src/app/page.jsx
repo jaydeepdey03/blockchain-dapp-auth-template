e1ba07693686c029a4977319718a730d04ea5424
Jaydeep <jaydeep.dey03@gmail.com>
2023-10-02 13:44:53 +0530

    updated home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,15 @@
 "use client";
-
+import {useRouter} from "next/navigation";
+import {Button} from "../components/ui/button";
 export default function Home() {
+  const router = useRouter();
   return (
     <div
-      className="h-screen w-screen grid place-items-center"
+      className="h-screen w-screen flex justify-center items-center flex-col"
       suppressHydrationWarning
     >
-      <p>home pages</p>
+      <p className="font-bold text-6xl m-4">Thirdweb Auth</p>
+      <Button onClick={() => router.push("/connectwallet")}>Get Started</Button>
     </div>
   );
 }
